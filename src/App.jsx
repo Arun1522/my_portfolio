7afa0cc863619f227ec1c1bc8a9df44a3728e3cd
Web-Astroswamig <info.astroswamig@gmail.com>
2022-10-01 12:55:32 +0530

    adding loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
@@ -10,18 +10,29 @@ import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 const App = () => {
+  const [loading, setloading] = useState(true);
+  const container = document.getElementById('container');
+
+  if (container) {
+    setTimeout(() => {
+      container.style.display = 'none';
+      setloading(false);
+    }, 2000);
+  }
   return (
-    <>
-      <Header />
-      <Nav />
-      <About />
-      <Experience />
-      <Services />
-      <Portfolio />
-      <Testimonials />
-      <Contact />
-      <Footer />
-    </>
+    !loading && (
+      <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </>
+    )
   );
 };
 
